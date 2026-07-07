04fb37373d90351b7b09eb0ee991fa2c5c960c48
manishcodesq <manishkulkarni375@gmail.com>
2026-07-07 13:41:50 +0530

    Update API base URL to use environment variable

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://banksphere-role-based-internal-banking.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((req) => {
