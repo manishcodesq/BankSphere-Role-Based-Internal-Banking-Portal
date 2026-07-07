90f002e0185b51dbc5f760bcac61f1de2e6f38ad
manishcodesq <manishkulkarni375@gmail.com>
2026-07-07 13:43:31 +0530

    Remove unused health check API call from main component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-useEffect(() => {
-  axios.get("https://banksphere-role-based-internal-banking.onrender.com/health");
-}, []);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
