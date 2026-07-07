bea2bc0764a8fdb2c25879c3b0793a3b827583c2
manishcodesq <manishkulkarni375@gmail.com>
2026-07-07 12:47:36 +0530

    Update API base URL to production endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://banksphere-role-based-internal-banking.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
